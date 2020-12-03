bf2ef513eb0511754a4fa3501f225ae58dd9d438
Soham Tamboli <sohamtamboli@gmail.com>
2020-12-03 20:03:30 +0530

    Updated eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    '@nuxtjs/eslint-config-javascript',
+    '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
